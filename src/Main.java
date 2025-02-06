95dcf25aec2552c1df4ddf67a68a37bf5268a5e6
Nolan Crone <crone.nolan@gmail.com>
2025-02-05 21:45:48 -0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //class TicketKiosk
+        //  main()
+        //      num yourAge
+        //      Output “How old are you?”
+        //      Input yourAge
+        //      if yourAge >= 21 then
+        //          Output “You get a wrist band!”
+        //      end if
+        //  return
+        //end class
+    }
+}
