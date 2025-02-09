e19cbd0e90230c4b9e4c0c5adf983e2d0894e8c7
Nolan Crone <crone.nolan@gmail.com>
2025-02-08 19:36:19 -0500

    finalized code, ran checks.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +12,13 @@ public class Main {
         //      end if
         //  return
         //end class
+        Scanner in = new Scanner(System.in);
+        int yourAge;//I could have done double here for someone who wanted to do the division, but for simplicity I did int.
+        System.out.println("How old are you?");
+        yourAge = in.nextInt();
+        in.nextLine();
+        if(yourAge >= 21 ) {
+            System.out.println("You get a wrist band!");
+        }
     }
 }
